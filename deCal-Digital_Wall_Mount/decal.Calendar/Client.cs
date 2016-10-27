6217d3e3a4cd42106b456236b12fc2faf8d3a607
Maame Apenteng <mapenten@mail.uoguelph.ca>
2016-10-27 12:33:12 -0400

    created client class

---
+++ deCal-Digital_Wall_Mount/decal.Calendar/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace decal.Calendar
+{
+    public class Client
+    {
+        private string _forename;
+        private string _surname;
+        private string _phoneNumber;
+
+        public String FirstName
+        {
+            get { return _forename; }
+        }
+
+        public String LastName
+        {
+            get { return _surname; }
+        }
+
+        public String PhoneNumber
+        {
+            get { return _phoneNumber; }
+        }
+
+        private static bool isInvalid(string str)
+        {
+            return (String.IsNullOrEmpty(str) || String.IsNullOrWhiteSpace(str));
+        }
+
+        public Client(string firstName, string lastName, string phoneNumber)
+        {
+            if(isInvalid(firstName))
+            {
+                throw new ArgumentNullException("firstName cannot be null or empty.");
+            }
+            else if(isInvalid(lastName))
+            {
+                throw new ArgumentNullException("lastName cannot be null or empty.");
+            }
+            else if(isInvalid(phoneNumber))
+            {
+                throw new ArgumentNullException("phoneNumber cannot be null or empty.");
+            }
+            else
+            {
+                _forename = firstName;
+                _surname = lastName;
+                _phoneNumber = phoneNumber;
+            }
+        }
+    }
+}
